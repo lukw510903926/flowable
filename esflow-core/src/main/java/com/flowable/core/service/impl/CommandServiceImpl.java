14db47488aa71a66bb15f9d51e382be343178d86
lukw510903926 <13507615840@163.com>
2017-12-25 23:19:02 +0800

    Update CommandServiceImpl.java

--- esflow-core/src/main/java/com/flowable/core/service/impl/CommandServiceImpl.java
+++ esflow-core/src/main/java/com/flowable/core/service/impl/CommandServiceImpl.java
@@ -1,12 +1,8 @@
 package com.flowable.core.service.impl;
 
-import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.flowable.engine.ManagementService;
-import org.flowable.task.api.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +15,7 @@ import com.flowable.core.bean.BizInfoConf;
 import com.flowable.core.service.BizInfoConfService;
 import com.flowable.core.service.CommandService;
 import com.flowable.core.service.IBizInfoService;
-import com.flowable.core.service.IProcessDefinitionService;
+import com.flowable.core.service.IProcessExecuteService;
 import com.flowable.core.util.flowable.CommonJumpTaskCmd;
 
 /**
@@ -40,7 +36,7 @@ public class CommandServiceImpl implements CommandService {
 	private IBizInfoService bizInfoService;
 
 	@Autowired
-	private IProcessDefinitionService processDefinitionService;
+	private IProcessExecuteService processExecuteService;
 
 	private Logger logger = LoggerFactory.getLogger(CommandServiceImpl.class);
 
@@ -49,48 +45,25 @@ public class CommandServiceImpl implements CommandService {
 	public void jumpCommand(String bizId, String taskId, String targetTaskDefKey) {
 
 		try {
+			BizInfo bizInfo = this.bizInfoService.get(bizId);
+			if (bizInfo == null) {
+				throw new ServiceException("工单不存在!工单id:{}" + bizId);
+			}
+			BizInfoConf conf = this.bizInfoConfService.getMyWork(bizId);
+			if (conf == null) {
+				throw new ServiceException("请确认是否有提交工单权限");
+			}
 			CommonJumpTaskCmd cmd = new CommonJumpTaskCmd(taskId, targetTaskDefKey);
 			managementService.executeCommand(cmd);
-			this.updateBizInfo(bizId, targetTaskDefKey);
+			BizInfoConf example = new BizInfoConf();
+			example.setBizInfo(bizInfo);
+			example.setTaskId(taskId);
+			List<BizInfoConf> confs = this.bizInfoConfService.findBizInfoConf(example);
+			processExecuteService.updateBizTaskInfo(bizInfo, conf);
+			this.bizInfoConfService.delete(confs);
 		} catch (Exception e) {
 			logger.error(" 流程跳转失败 : {}", e);
 			throw new ServiceException("流程跳转失败!");
-		}
-	}
-
-	private void updateBizInfo(String bizId, String targetTaskDefKey) {
-
-		BizInfo bizInfo = this.bizInfoService.get(bizId);
-		if(bizInfo == null){
-			return;
-		}
-		BizInfoConf example = new BizInfoConf();
-		example.setBizInfo(bizInfo);
-		List<BizInfoConf> confs = this.bizInfoConfService.findBizInfoConf(example);
-		this.bizInfoConfService.delete(confs);
-		List<Task> list = processDefinitionService.getNextTaskInfo(bizInfo.getProcessInstanceId());
-		if (!CollectionUtils.isEmpty(list)) {
-			String taskIds = "";
-			String assignee = "";
-			for (Task task : list) {
-				BizInfoConf conf = new BizInfoConf();
-				conf.setTaskAssignee(task.getAssignee());
-				conf.setBizInfo(bizInfo);
-				conf.setCreateTime(new Date());
-				conf.setTaskId(task.getId());
-				this.bizInfoConfService.save(conf);
-				taskIds = taskIds + task.getId() + ",";
-				if (StringUtils.isNotBlank(task.getAssignee())) {
-					assignee = assignee + task.getAssignee() + ",";
-				}
-			}
-			bizInfo.setTaskId(taskIds);
-			assignee = StringUtils.isBlank(assignee) ? null : assignee.substring(0, assignee.lastIndexOf(","));
-			bizInfo.setTaskAssignee(assignee);
-			bizInfo.setTaskDefKey(targetTaskDefKey);
-			bizInfo.setStatus(list.get(0).getName());
-			bizInfo.setTaskName(list.get(0).getName());
-			this.bizInfoService.update(bizInfo);
 		}
 	}
 }
