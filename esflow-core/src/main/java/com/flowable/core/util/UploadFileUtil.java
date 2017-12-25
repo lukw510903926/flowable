95ca29641d0f000162aa2d02cef69e54b17763b8
lkw510903926 <13507615840@.163>
2017-12-25 15:19:33 +0800

    文件上传

--- esflow-core/src/main/java/com/flowable/core/util/UploadFileUtil.java
+++ esflow-core/src/main/java/com/flowable/core/util/UploadFileUtil.java
@@ -8,6 +8,8 @@ import java.util.UUID;
 
 import javax.imageio.ImageIO;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.flowable.common.utils.DateUtils;
@@ -15,66 +17,71 @@ import com.flowable.core.bean.BizFile;
 
 /**
  * 文件上传工具
- * @author 26223
  *
+ * @author 26223
  */
 public class UploadFileUtil {
-//	private static String rootPath = "E:\\temp\\workfile\\";
-	private static String rootPath = "/home/ipnet/esflowFilePath/";
-	/**
-	 * 将上传的文件保存到磁盘，文件目录为yyyyMM/dd/yyyyMMddHHmmssSSS
-	 * 
-	 * @param file
-	 * @return
-	 */
-	public static BizFile saveFile(MultipartFile file) {
-		if (file == null || file.getSize() == 0) {
-			return null;
-		}
-		Date date = new Date();
-		String filePath2 = DateUtils.formatDate(date, "yyyyMM") + File.separator + DateUtils.formatDate(date, "dd") + File.separator;
-//		filePath2 = filePath2 + DateUtils.formatDate(date, "yyyyMMddHHmmssSSS");
-		filePath2 = filePath2 + UUID.randomUUID().toString().replaceAll("-","");
-		String dp = file.getOriginalFilename();
-		dp = dp.substring(dp.lastIndexOf(".") + 1);
-		filePath2 = filePath2 + "." + dp;
-		String filePath = rootPath + filePath2;
-		File pFile = new File(filePath);
-		pFile.getParentFile().mkdirs();
-		BizFile bean = new BizFile();
-		try { 
-			file.transferTo(pFile);
-			bean.setCreateDate(date);
-			bean.setPath(filePath2);
-			bean.setName(file.getOriginalFilename());
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		if(bean!=null){
-			//检测是否为图片
-			try {  
-			    Image image=ImageIO.read(pFile);  
-			    if (image == null) {  
-			    	bean.setFileType("FILE");
-			    }  
-			    else{
-			    	bean.setFileType("IMAGE");
-			    }
-			} catch(IOException ex) {    
-			}
-		}
-		return bean;
-	}
-	/**
-	 * 获取到上传的文件
-	 * @param bean
-	 * @return
-	 */
-	public static File getUploadFile(BizFile bean){
-		String fielPath = rootPath+bean.getPath();
-		File file = new File(fielPath);
-		return file;
-	}
+    //	private static String rootPath = "E:\\temp\\workfile\\";
+    private static String rootPath = "/home/ipnet/esflowFilePath/";
+
+    private static Logger logger = LoggerFactory.getLogger(UploadFileUtil.class);
+
+    /**
+     * 将上传的文件保存到磁盘，文件目录为yyyyMM/dd/yyyyMMddHHmmssSSS
+     *
+     * @param file
+     * @return
+     */
+    public static BizFile saveFile(MultipartFile file) {
+
+        if (file == null || file.getSize() == 0) {
+            return null;
+        }
+        Date date = new Date();
+        String filePath2 = DateUtils.formatDate(date, "yyyyMM") + File.separator + DateUtils.formatDate(date, "dd") + File.separator;
+        filePath2 = filePath2 + UUID.randomUUID().toString().replaceAll("-", "");
+        String dp = file.getOriginalFilename();
+        dp = dp.substring(dp.lastIndexOf(".") + 1);
+        filePath2 = filePath2 + "." + dp;
+        String filePath = rootPath + filePath2;
+        File pFile = new File(filePath);
+        pFile.getParentFile().mkdirs();
+        BizFile bean = new BizFile();
+        try {
+            file.transferTo(pFile);
+            bean.setCreateDate(date);
+            bean.setPath(filePath2);
+            bean.setName(file.getOriginalFilename());
+        } catch (IllegalStateException e) {
+            logger.error("文件上传失败 : {}", e);
+        } catch (IOException e) {
+            logger.error("文件上传失败 : {}", e);
+        }
+        if (bean != null) {
+            try {
+                Image image = ImageIO.read(pFile);
+                if (image == null) {
+                    bean.setFileType("FILE");
+                } else {
+                    bean.setFileType("IMAGE");
+                }
+            } catch (IOException ex) {
+                bean.setFileType("FILE");
+                logger.error("获取文件类型失败 : {}", ex);
+            }
+        }
+        return bean;
+    }
+
+    /**
+     * 获取到上传的文件
+     *
+     * @param bean
+     * @return
+     */
+    public static File getUploadFile(BizFile bean) {
+        String fielPath = rootPath + bean.getPath();
+        File file = new File(fielPath);
+        return file;
+    }
 }
