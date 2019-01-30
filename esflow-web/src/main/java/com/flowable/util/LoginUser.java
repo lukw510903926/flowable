a0f3bd2b17c09469239cb5059631e116dfb260fd
yangqi <13507615840@163.com>
2019-01-30 16:33:55 +0800

    包修改

---
+++ esflow-web/src/main/java/com/flowable/util/LoginUser.java
@@ -0,0 +1,40 @@
+package com.flowable.util;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LoginUser implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String username;
+	
+	private String name;
+	
+	private Set<String> roles = new HashSet<String>();
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<String> roles) {
+		this.roles = roles;
+	}
+}
