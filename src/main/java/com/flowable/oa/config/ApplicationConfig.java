eec5a61ac7c9afaa79d5530ec578e9598a0a54a2
lkw510903926 <13507615840@163.com>
2019-02-01 22:23:17 +0800

    orm 框架改为mybatis

--- src/main/java/com/flowable/oa/config/ApplicationConfig.java
+++ src/main/java/com/flowable/oa/config/ApplicationConfig.java
@@ -1,11 +1,8 @@
 package com.flowable.oa.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import tk.mybatis.mapper.autoconfigure.MapperCacheDisabler;
 
 /**
  * 系统配置
@@ -22,11 +19,5 @@ public class ApplicationConfig implements WebMvcConfigurer{
 
 		LoginInterceptor loginInterceptor = new LoginInterceptor();
 		registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
-	}
-
-	@Bean
-	@Profile("dev")
-	public MapperCacheDisabler mapperCacheDisabler() {
-		return new MapperCacheDisabler();
 	}
 }
