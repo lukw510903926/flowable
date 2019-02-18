6f098525e6fd3c7fcc3afe9c6f3b273086173f82
yangqi <13507615840@163.com>
2019-02-18 11:37:31 +0800

    工单详情展示

--- src/main/java/com/flowable/oa/controller/BizInfoController.java
+++ src/main/java/com/flowable/oa/controller/BizInfoController.java
@@ -126,6 +126,14 @@ public class BizInfoController {
         return "modules/biz/biz_detail";
     }
 
+    @RequestMapping(value = "bizInfo/{id}", method = RequestMethod.GET)
+    public String detail(@PathVariable String id, Model model) {
+
+        model.addAttribute("id", id);
+        model.addAttribute("currentUser", JSONObject.toJSON(WebUtil.getLoginUser()));
+        return "modules/biz/bizInfo_detail";
+    }
+
     @ResponseBody
     @GetMapping("/bizInfo/detail/{bizId}")
     public BizInfoVo detail(@PathVariable("bizId")String bizId){
