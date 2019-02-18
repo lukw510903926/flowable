0b7a33217172d186de3c1c320f9be08134fe4e8c
yangqi <13507615840@163.com>
2019-02-18 09:21:28 +0800

    jquery 工具类

--- src/main/resources/static/js/common/jquery-utils.js
+++ src/main/resources/static/js/common/jquery-utils.js
@@ -5,8 +5,8 @@
          * 清空表单
          */
         clearForm: function () {
-            let $this = this;
-            $($this).find('[name]').each(function () {
+            let _this = this;
+            $(_this).find('[name]').each(function () {
                 $(this).val('');
             });
         },
@@ -15,8 +15,8 @@
          */
         getForm: function () {
             let param = {};
-            let $this = this;
-            $($this).find('[name]').each(function () {
+            let _this = this;
+            $(_this).find('[name]').each(function () {
                 let value = $(this).val();
                 if (value) {
                     param[this.name] = $(this).val();
@@ -29,8 +29,8 @@
          * @param params
          */
         setForm: function (params) {
-            let $this = this;
-            $($this).find('[name]').each(function () {
+            let _this = this;
+            $(_this).find('[name]').each(function () {
                 $(this).val(params[this.name])
             });
         },
@@ -42,10 +42,10 @@
          */
         validateForm: function () {
 
-            let $this = this;
+            let _this = this;
             let checkEmpty = true;
             let defaultMsg = '表单校验失败';
-            $($this).find('[name]').each(function () {
+            $(_this).find('[name]').each(function () {
                 $(this).siblings("i").remove();
                 if (this.required && !$(this).validateRule()) {
                     checkEmpty = false;
@@ -58,21 +58,21 @@
 
         validateRule: function () {
 
-            let ele = $(this);
+            let _this = $(this);
             let result = false;
-            let type = ele.type;
+            let type = _this.type;
             switch (type) {
                 case 'text':
-                    result = ele.text();
+                    result = _this.text();
                     break;
                 case  'email':
-                    result = ele.email();
+                    result = _this.email();
                     break;
                 case 'number':
-                    result = ele.checkNum();
+                    result = _this.checkNum();
                     break;
                 default:
-                    result = ele.text();
+                    result = _this.text();
             }
             return result;
         },
@@ -83,14 +83,14 @@
          */
         text: function () {
 
-            let ele = $(this);
+            let _this = $(this);
             let _result = true;
-            let _val = $.trim($(ele).val());
+            let _val = $.trim($(_this).val());
             if (_val === '') {
                 _result = false;
             }
-            let _min = ele.attr('minLength');
-            let _max = ele.attr('max_Length');
+            let _min = _this.attr('minLength');
+            let _max = _this.attr('max_Length');
             if (_min && _max) {
                 _result = (_val.length >= _min && _val.length <= _max);
             }
@@ -100,7 +100,7 @@
             if (!_min && _max) {
                 _result = (_val.length <= _max);
             }
-            let _pattern = ele.attr('pattern');
+            let _pattern = _this.attr('pattern');
             if (_pattern) {
                 let _reg = new RegExp(_pattern);
                 _result = _reg.test(_val) && _result;
@@ -113,15 +113,15 @@
          */
         email: function () {
 
-            let ele = $(this);
+            let _this = $(this);
             let _result = true;
-            let _val = $.trim(ele.val());
-            let _pattern = ele.attr('pattern');
+            let _val = $.trim(_this.val());
+            let _pattern = _this.attr('pattern');
             if (!_pattern) {
                 let _reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
                 _result = _reg.test(_val);
             } else {
-                _result = ele.text();
+                _result = _this.text();
             }
             return _result;
         },
@@ -131,8 +131,7 @@
          * @returns {boolean}
          */
         checkNum: function () {
-            let ele = $(this);
-            return !/^((([1-9]+\d*)?|\d?)(\.\d*)?)?$/.test(ele.value);
+            return !/^((([1-9]+\d*)?|\d?)(\.\d*)?)?$/.test($(this).value);
         },
 
         /**
