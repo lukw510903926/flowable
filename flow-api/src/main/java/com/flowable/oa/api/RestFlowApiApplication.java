f6e37d328c5894cec13bb75159c4d0db5afd2662
lkw510903926 <13507615840@163.com>
2019-02-22 22:26:42 +0800

    模块拆分

--- flow-api/src/main/java/com/flowable/oa/api/RestFlowApiApplication.java
+++ flow-api/src/main/java/com/flowable/oa/api/RestFlowApiApplication.java
@@ -1,5 +1,11 @@
 package com.flowable.oa.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
 /**
  * <p>
  *
@@ -8,5 +14,15 @@ package com.flowable.oa.api;
  * @email 13507615840@163.com
  * @since 2019/2/22 22:00
  **/
+@SpringBootApplication
+@MapperScan(basePackages = "com.flowable.oa.core.dao")
 public class RestFlowApiApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(RestFlowApiApplication.class);
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RestFlowApiApplication.class, args);
+        logger.info("flow rest api application start successfully==========");
+    }
 }
