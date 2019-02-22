461f809f93805ba7e4d1eb48b860e42ced27b828
yangqi <13507615840@163.com>
2019-02-22 11:50:18 +0800

    前端优化

--- src/main/resources/static/modules/index/menu_index.js
+++ src/main/resources/static/modules/index/menu_index.js
@@ -8,14 +8,19 @@ menu.index = {
         var currentPath = window.document.location.pathname;
         currentPath = currentPath.split(';')[0];
         var tag = $('ul').find('[href="' + currentPath + '"]');
-        tag.css('background', '#1890ff');
-        tag.parent().parent().removeClass('collapse');
-        $('#'+tag.parent().parent().attr('id')+'_i').removeClass('icon-chevron-down').addClass('icon-chevron-up');
+        if (tag.length) {
+            tag.css('background', '#1890ff');
+            tag.parent().parent().removeClass('collapse');
+            $('#' + tag.parent().parent().attr('id') + '_i').removeClass('icon-chevron-down').addClass('icon-chevron-up');
+        } else {
+            $('#ul1').removeClass('collapse');
+            $('#ul1_i').removeClass('icon-chevron-down').addClass('icon-chevron-up');
+        }
         menu.index.restHeight();
         window.addEventListener("resize", menu.index.restHeight);
     },
 
-    restHeight:function(){
+    restHeight: function () {
         var height = $(window).height() - 60;
         height = Math.max(height, $('.right_content').height());
         $('.leftMenu').css({"height": height + 'px', "backgroundColor": '#001529'});
