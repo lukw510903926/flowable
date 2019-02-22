e2def20dec9f3d2b2110bb5d316627a568c99f72
lkw510903926 <13507615840@163.com>
2019-02-22 22:15:06 +0800

    模块拆分

---
+++ flow-api/src/main/java/com/flowable/oa/api/RestFlowApiApplication.java
@@ -0,0 +1,12 @@
+package com.flowable.oa.api;
+
+/**
+ * <p>
+ *
+ * @author yangqi
+ * @Description </p>
+ * @email 13507615840@163.com
+ * @since 2019/2/22 22:00
+ **/
+public class RestFlowApiApplication {
+}
