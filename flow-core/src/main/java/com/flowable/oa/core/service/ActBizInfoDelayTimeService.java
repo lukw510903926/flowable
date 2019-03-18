7b6bc331e563ee5e3c395aa98ec95204b12fca86
lkw510903926 <13507615840@163.com>
2019-03-18 17:10:53 +0800

    代码优化

--- flow-core/src/main/java/com/flowable/oa/core/service/ActBizInfoDelayTimeService.java
+++
@@ -1,9 +0,0 @@
-package com.flowable.oa.core.service;
-
-
-import com.flowable.oa.core.entity.ActBizInfoDelayTime;
-import com.flowable.oa.core.util.mybatis.IBaseService;
-
-public interface ActBizInfoDelayTimeService extends IBaseService<ActBizInfoDelayTime> {
-
-}
