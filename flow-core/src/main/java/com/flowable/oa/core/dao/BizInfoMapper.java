c9e3f68ff5f09713797e395979787e5a21a132ec
yangqi <2622319737@qq.com>
2020-02-20 18:02:14 +0800

    代码注释修改

--- flow-core/src/main/java/com/flowable/oa/core/dao/BizInfoMapper.java
+++ flow-core/src/main/java/com/flowable/oa/core/dao/BizInfoMapper.java
@@ -2,8 +2,6 @@ package com.flowable.oa.core.dao;
 
 import com.flowable.oa.core.entity.BizInfo;
 import com.flowable.oa.core.util.mybatis.MyMapper;
-import com.flowable.oa.core.vo.BizInfoVo;
-import java.util.List;
 
 /**
  * <p>
@@ -15,5 +13,4 @@ import java.util.List;
  **/
 public interface BizInfoMapper extends MyMapper<BizInfo> {
 
-	List<BizInfo> queryWorkOrder(BizInfoVo bizInfoVo);
 }
